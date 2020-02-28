d6c366b892dce04b5c32f788734afc1dd77a9060
Lily <yulex.42@gmail.com>
2020-02-28 17:45:06 -0400

    updated tiling script

--- tile.js
+++ tile.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const base_size = 4096;
-const tile_size = 256;
-tile(0);
-function tile(zoom) {
-    const numTiles = 2**zoom;
-    const size = tile_size*numTiles;
-    if (size < base_size) {
-        tile(zoom+1);
-    }
-
-    console.log(zoom, size);
+const maxLevel = 5;
+const tileSize = 256;
+execSync('rm -r tiles');
+for (let z = 0; z <= maxLevel; z++) {
+    const numTiles = 2**z;
+    const size = tileSize*numTiles;
+    execSync(`inkscape -z -e base.png -w ${size} -h ${size} base.svg`);
     for (let x = 0; x < numTiles; x++) {
         for (let y = 0; y < numTiles; y++) {
-            fs.mkdirSync(`./tiles/${zoom}/${x}/`, {recursive:true});
-            //exec(`touch './tiles/${zoom}/${x}/${y}.png'`);
-            console.log(`./tiles/${zoom}/${x}/${y}.png`);
-            execSync(`convert base.png -resize ${size} -crop 256x256+${x*tile_size}+${y*tile_size} +repage ./tiles/${zoom}/${x}/${y}.png`);
+            fs.mkdirSync(`./tiles/${z}/${x}/`, {recursive:true});
+            console.log(`./tiles/${z}/${x}/${y}.png`);
+            execSync(`convert base.png -crop 256x256+${x*tileSize}+${y*tileSize} +repage ./tiles/${z}/${x}/${y}.png`);
         }
     }
 }
